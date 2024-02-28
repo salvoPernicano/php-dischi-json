a97a7d8c8f6f7ed4a3578669795cc7f65da32aa6
salvoPernicano <salvo.pernicano@gmail.com>
2024-02-28 16:52:30 +0100

    creazione cards

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,14 +9,26 @@
 </head>
 
 <body>
-<div id="app">
+    <div id="app">
+        <div class="container">
+            <div class="diskCard" v-for="(element,index) in arrayDischi">
+                <figure>
+    <img :src="element.poster" alt="">
+                </figure>
+                <div class="cardInfo">
+                    <h2>{{element.title}}</h2>
+                    <h3>{{element.author}}</h3>
+                    <span>{{element.year}}</span>
+                </div>
+            </div>
+        </div>
 
-
-</div>
+    </div>
 
 
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="./js/main.js"></script>
 </body>
+
 </html>
